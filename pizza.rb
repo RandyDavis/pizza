4df3941074738de291c73b0b36ab5cd5cf422c0b
vagrant <vagrant@student-dev-box>
2014-07-16 11:12:56 -0500

    Add keyword argument for vegetarian option in initialize method.

--- pizza.rb
+++ pizza.rb
@@ -2,9 +2,17 @@ class Pizza
 end
 
 class Topping
-  attr_accessor :name
-  def initialize(name)
+  # Instantiate a new topping.
+  #
+  # name       - The String name of the topping.
+  # vegetarian - The Boolean indicating whether or not the topping is
+  #              vegetarian.
+  #
+  # Returns the new Topping.
+  attr_accessor :name, :vegetarian
+  def initialize(name, vegetarian: false)
     @name = name
+    @vegetarian = vegetarian
   end
 
 end
